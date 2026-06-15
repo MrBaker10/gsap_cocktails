d7fe021be2cba9085ccff9f8c6c2fba1814cc313
Sascha <baecker.sascha@googlemail.com>
2026-06-15 11:47:00 +0200

    Implement Hero Scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { ScrollTrigger, SplitText } from "gsap/all";
+import {ScrollTrigger, SplitText} from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -6,12 +6,11 @@ import Hero from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return (
-    <main>
-      <Navbar />
-        <Hero />
-        <div className="h-dvh bg-black"></div>
-    </main>
-  );
+    return (
+        <main>
+            <Navbar/>
+            <Hero/>
+        </main>
+    );
 };
 export default App;
